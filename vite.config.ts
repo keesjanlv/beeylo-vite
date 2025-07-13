5e8a074f8b9b43b913ce0308b5239c1de82bcb9c
keesjanlv <keesjanvg@gmail.com>
2025-07-13 10:17:47 +0200

    Fix: Uitgebreide headers + Vite output config voor Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
         manualChunks: undefined,
       }
     }
